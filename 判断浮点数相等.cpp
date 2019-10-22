f1890cd910b7be4698b40aba64dd20c2a802d47f
lidonghao <1270313527@qq.com>
2019-10-22 09:10:59 +0800

    Update 判断浮点数相等.cpp

--- 判断浮点数相等.cpp
+++ 判断浮点数相等.cpp
@@ -6,9 +6,8 @@ int main()
 float m,x,y,a,b;
  scanf("%f %f %f",&m,&x,&y);//for example 1.78 1.779999 2 
  a=m/y,b=x/y;
- a=(int)(a*100000+0.5)/100000;
- b=(int)(b*100000)/100000;
- if(a==b){
+
+ if(a-b<0.00001 && a-b>-0.00001){
    printf("equal");
 }
   else printf("not equal");
